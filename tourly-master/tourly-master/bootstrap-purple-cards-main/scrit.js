f1555f523777ff2fc473c6404179621d7a46f66d
Agustine <agustineagt23417@gmail.com>
2025-06-30 12:49:23 +0530

    Initial commit

---
+++ tourly-master/tourly-master/bootstrap-purple-cards-main/scrit.js
@@ -0,0 +1,6 @@
+document.getElementById("menu-btn").addEventListener("click", () => {
+  document.getElementById("mobile-menu").style.display = "flex";
+});
+document.getElementById("close-menu").addEventListener("click", () => {
+  document.getElementById("mobile-menu").style.display = "none";
+});
